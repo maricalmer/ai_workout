be1bfba5662d539ab034e34752b2a4cd89967ae6
Mari Calmer <superlassig@gmail.com>
2025-05-18 23:00:26 +0200

    feat: add openai gem + seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Workout.destroy_all
+
+Workout.create(equipment: "bench", focus: "full body")
+Workout.create(equipment: "nothing", focus: "back")
